aa744511998f771dcdc36642a167d2090def590d
Dariusz Kolawa <ddkolawa@outlook.com>
2020-12-02 19:19:50 +0100

    nauka

---
+++ asp2.7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asp2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
